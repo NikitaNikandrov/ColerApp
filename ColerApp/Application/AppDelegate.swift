210cf63a3ae582839af806b17f0135dc568bcdce
Nikita Nikandrov <nnikandroff@yandex.ru>
2024-07-07 18:09:04 +0300

    feature: added navBar, changed statusBar

--- ColerApp/Application/AppDelegate.swift
+++ ColerApp/Application/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         let mainViewController = ViewController()
-        window?.rootViewController = mainViewController
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         return true
     }
--- ColerApp/ViewController.swift
+++ ColerApp/ViewController.swift
@@ -8,10 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @objc private func heartButtonTapped() {
+        print("Heart button tapped")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .green
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .lightGray
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        navigationItem.title = "Coler App"
+        
+        navigationController?.navigationBar.tintColor = .white
+        
+        let heartImage = UIImage(systemName: "heart.fill")
+        let rightBarButtonItem = UIBarButtonItem(image: heartImage, style: .plain, target: self, action: #selector(heartButtonTapped))
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        setStatusBarBackgroundColor(color: .lightGray)
+    }
+    
+    private func setStatusBarBackgroundColor(color: UIColor) {
+        guard let statusBarFrame = view.window?.windowScene?.statusBarManager?.statusBarFrame else { return }
+        
+        let statusBarView = UIView(frame: statusBarFrame)
+        statusBarView.backgroundColor = color
+        view.addSubview(statusBarView)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 }
 
