1e90536a2d52e599d0545b60604854a9c8981079
Nikita Nikandrov <nnikandroff@yandex.ru>
2024-07-11 17:03:59 +0300

    update .gitignore

--- ColerApp/Model/RALColor.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RALColor.swift
-//  ColerApp
-//
-//  Created by Nikita Nikandrov on 11.07.2024.
-//
-
-import Foundation
---
+++ ColerApp/Model/RALColorModel.swift
@@ -0,0 +1,16 @@
+//
+//  RALColor.swift
+//  ColerApp
+//
+//  Created by Nikita Nikandrov on 11.07.2024.
+//
+
+import Foundation
+
+struct RALColor {
+    let ral: String
+    let name: String
+    let r: CGFloat
+    let g: CGFloat
+    let b: CGFloat
+}
