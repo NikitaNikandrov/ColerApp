d450a3461613fc970193840ca82c4e87e854aa5e
Nikita Nikandrov <nnikandroff@yandex.ru>
2024-07-11 16:13:16 +0300

    Save current changes RAL Color

---
+++ ColerApp/Model/RALColor.swift
@@ -0,0 +1,8 @@
+//
+//  RALColor.swift
+//  ColerApp
+//
+//  Created by Nikita Nikandrov on 11.07.2024.
+//
+
+import Foundation
---
+++ ColerApp/Presenter/CameraViewPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  CameraViewPresenter.swift
+//  ColerApp
+//
+//  Created by Nikita Nikandrov on 07.07.2024.
+//
+
+import Foundation
